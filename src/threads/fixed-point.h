76c3253b429e9148c1101624d18d5ec1c098cbe9
bc522 <bc522@ic.ac.uk>
2024-10-11 12:40:25 +0100

    added comments to macros

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -1,32 +1,41 @@
 #ifndef THREADS_FIXED_POINT_H
 #define THREADS_FIXED_POINT_H
 
-//not sure if this will have to change
+/* Set scaling factor used in fixed-point arithmetic */
 #define N_FRACTIONAL_BITS 14
 #define FACTOR (2^N_FRACTIONAL_BITS)
 
+/* Converts an integer to a fixed-point value */
 #define INT_TO_FIXED(n) ((n) * FACTOR)
 
-// fixed point number x to integer rounding towards 0
+/* Converts a fixed-point value to an integer via truncating */
 #define FIXED_TO_INT_TRUNC(x) ((x) / FACTOR)
 
-// fixed point number x to integer rounding to the nearest int
+/* Converts a fixed-point value to an integer via rounding */
 #define FIXED_TO_INT(x) (((x) >= 0) ? ((x) + FACTOR / 2) / FACTOR : ((x) - FACTOR / 2) / FACTOR)
 
+/* Adds two fixed-point values */
 #define FIXED_ADD(x, y) ((x) + (y))
 
+/* Adds a fixed-point value and an integer */
 #define FIXED_ADD_INT(x, n) ((x) + ((n) * FACTOR))
 
+/* Subtracts a fixed-point value from another */
 #define FIXED_SUB(x, y) ((x) - (y))
 
+/* Subtracts an integer from a fixed-point value */
 #define FIXED_SUB_INT(x, n) ((x) - ((n) * FACTOR))
 
+/* Multiplies two fixed-point values */
 #define FIXED_MUL(x, y) (((int64_t) x) * (y) / FACTOR)
 
+/* Multiplies a fixed-point value with an integer */
 #define FIXED_MUL_INT(x, n) ((x) * (n))
 
+/* Divides two fixed-point values */
 #define FIXED_DIV(x, y) (((int64_t) x) * FACTOR / (y))
 
+/* Divides a fixed-point value by an integer */
 #define FIXED_DIV_INT(x, n) ((x) / (n))
 
 #endif /* threads/fixed-point.h */
