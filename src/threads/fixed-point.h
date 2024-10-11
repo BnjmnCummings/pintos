d1c684ecae76649c876c2504545804972702848c
bc522 <bc522@ic.ac.uk>
2024-10-11 12:34:01 +0100

    completed all fixed point arithmetic macros

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -3,14 +3,30 @@
 
 //not sure if this will have to change
 #define N_FRACTIONAL_BITS 14
-#define SCALE_FACTOR (2^N_FRACTIONAL_BITS)
+#define FACTOR (2^N_FRACTIONAL_BITS)
 
-#define INT_TO_FXP(n) (n * SCALE_FACTOR)
+#define INT_TO_FIXED(n) ((n) * FACTOR)
 
 // fixed point number x to integer rounding towards 0
-#define FXP_TO_INT0(x) (x / SCALE_FACTOR)
+#define FIXED_TO_INT_TRUNC(x) ((x) / FACTOR)
 
 // fixed point number x to integer rounding to the nearest int
-#define FXP_TO_INT(x) //todo
+#define FIXED_TO_INT(x) (((x) >= 0) ? ((x) + FACTOR / 2) / FACTOR : ((x) - FACTOR / 2) / FACTOR)
+
+#define FIXED_ADD(x, y) ((x) + (y))
+
+#define FIXED_ADD_INT(x, n) ((x) + ((n) * FACTOR))
+
+#define FIXED_SUB(x, y) ((x) - (y))
+
+#define FIXED_SUB_INT(x, n) ((x) - ((n) * FACTOR))
+
+#define FIXED_MUL(x, y) (((int64_t) x) * (y) / FACTOR)
+
+#define FIXED_MUL_INT(x, n) ((x) * (n))
+
+#define FIXED_DIV(x, y) (((int64_t) x) * FACTOR / (y))
+
+#define FIXED_DIV_INT(x, n) ((x) / (n))
 
 #endif /* threads/fixed-point.h */
