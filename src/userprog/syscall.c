3d2f84637223c3190cc788a78e274e255ba6e028
bc2423 <bc2423@ic.ac.uk>
2024-11-11 14:56:04 +0000

    fix: fixed pointer casting for stack pointer

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -20,11 +20,10 @@ syscall_handler (struct intr_frame *f UNUSED)
   //Pre-Requisite we need to be able to read/write data from the User Virtual Adress Space.
 
   //validate the alleged pointer to System Call Number.
-  // any invalid pointers/ edge cases should terminate the user process
+
   if (is_user_vaddr(f->esp)) {
-    // not sure if we need to do more with this stack pointer
-    int32_t sys_call_number = (int32_t) f->esp;
-    printf ("System Call! System Call Number: %d\n", sys_call_number);
+    int32_t *sys_call_number = (int32_t *) f->esp;
+    printf ("System Call! System Call Number: %d\n", *sys_call_number);
 
   }else {
     printf ("invalid memory address!\n");
