b5c9af5c601fe070b0a2df705471fc201574f93b
bc2423 <bc2423@ic.ac.uk>
2024-11-11 16:14:11 +0000

    feat: sys-handler function prototypes and a switch statement for handling system calls by argument

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -16,16 +16,37 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  //TODO retrieve the system call number from the User Stack
-  //Pre-Requisite we need to be able to read/write data from the User Virtual Adress Space.
-
-  //validate the alleged pointer to System Call Number.
-
   if (is_user_vaddr(f->esp)) {
     int32_t *sys_call_number = (int32_t *) f->esp;
-    printf ("System Call! System Call Number: %d\n", *sys_call_number);
 
-  }else {
+    switch(*sys_call_number) {
+    /* void handlers */
+      /* void arguemnt*/
+      case SYS_HALT:
+      /* single argument*/
+      case SYS_EXIT:
+      case SYS_CLOSE:
+      case SYS_SEEK:
+    /* handlers with a return value*/
+      /* single argument*/
+      case SYS_EXEC:  
+      case SYS_WAIT:  
+      case SYS_TELL:    
+      case SYS_OPEN:    
+      case SYS_FILESIZE:
+      case SYS_REMOVE: 
+      /* two arguments */ 
+      case SYS_CREATE: 
+      /* three arguemnts */
+      case SYS_READ:    
+      case SYS_WRITE:   
+         printf ("System Call! System Call Number: %d\n", *sys_call_number);
+         break;
+      default:
+        printf ("Unsupported System Call\n");
+        thread_exit ();
+    }
+  } else {
     printf ("invalid memory address!\n");
     thread_exit ();
   }
